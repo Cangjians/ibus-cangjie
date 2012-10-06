b2e02bc4c953e7e5ec3618c92815ec26a7ae14b1
Mathieu Bridon <bochecha@fedoraproject.org>
2012-10-06 22:28:38 +0800

    Add the manual script
    This is not very interesting, as it's not how the IBus daemon
    loads our engine. However, it's good to have it for tests.
    
    And it seems some stuff loads engines without the IBus daemon.
    
    It's not correct at the moment though, as it only loads the
    component, but doesn't add the engine. We'll figure something
    out to load them from the XML, to avoid duplicating the code.

---
+++ src/main.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2012 - The IBus Cangjie authors
+#
+# This file is part of ibus-cangjie, the IBus Cangjie input method engine. 
+#
+# ibus-cangjie is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ibus-cangjie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ibus-cangjie.  If not, see <http://www.gnu.org/licenses/>.
+
+# Note: This script is only used to manually start the engine, the IBus daemon
+# will normally just load the XML file.
+
+import os
+import sys
+import getopt
+import ibus
+import factory
+import gobject
+import locale
+
+class IMApp:
+    def __init__(self, exec_by_ibus):
+        self.__component = ibus.Component("org.freedesktop.IBus.Cangjie",
+                                          "Cangjie Component",
+                                          "0.1.0",
+                                          "LGPLv3+",
+                                          "The IBus Cangjie authors")
+
+        # TODO: Add the engines, based on the XML file
+
+        self.__mainloop = gobject.MainLoop()
+        self.__bus = ibus.Bus()
+        self.__bus.connect("disconnected", self.__bus_disconnected_cb)
+        self.__factory = factory.EngineFactory(self.__bus)
+        if exec_by_ibus:
+            self.__bus.request_name("org.freedesktop.IBus.Cangjie", 0)
+        else:
+            self.__bus.register_component(self.__component)
+
+    def run(self):
+        self.__mainloop.run()
+
+    def __bus_disconnected_cb(self, bus):
+        self.__mainloop.quit()
+
+
+def launch_engine(exec_by_ibus):
+    IMApp(exec_by_ibus).run()
+
+def print_help(out, v = 0):
+    print >> out, "-i, --ibus             executed by ibus."
+    print >> out, "-h, --help             show this message."
+    print >> out, "-d, --daemonize        daemonize ibus"
+    sys.exit(v)
+
+def main():
+    try:
+        locale.setlocale(locale.LC_ALL, "")
+    except:
+        pass
+
+    exec_by_ibus = False
+    daemonize = False
+
+    shortopt = "ihd"
+    longopt = ["ibus", "help", "daemonize"]
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], shortopt, longopt)
+    except getopt.GetoptError, err:
+        print_help(sys.stderr, 1)
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print_help(sys.stdout)
+        elif o in ("-d", "--daemonize"):
+            daemonize = True
+        elif o in ("-i", "--ibus"):
+            exec_by_ibus = True
+        else:
+            print >> sys.stderr, "Unknown argument: %s" % o
+            print_help(sys.stderr, 1)
+
+    if daemonize:
+        if os.fork():
+            sys.exit()
+
+    launch_engine(exec_by_ibus)
+
+if __name__ == "__main__":
+    main()
