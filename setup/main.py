48df842e8dd4885a6216976cbb0e11a521ccd85f
Mathieu Bridon <bochecha@fedoraproject.org>
2012-10-11 21:24:18 +0800

    Reuse the same setup for Cangjie and Quick
    They will have the same set of options, so there's no need to
    duplicate the code.

--- setup/main.py
+++ setup/main.py
@@ -16,6 +16,7 @@
 # along with ibus-cangjie.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import argparse
 import gettext
 import locale
 import sys
@@ -35,8 +36,10 @@ punctuation_options = {0: "Chinese (full width)", 1: "English (half width)"}
 
 
 class Setup(object):
-    def __init__ (self, bus):
+    def __init__ (self, bus, engine):
         self.__bus = bus
+        self.__config_section = "engine/%s" % engine.capitalize()
+
         self.__config = self.__bus.get_config()
         self.__config.connect("value-changed", self.on_value_changed, None)
 
@@ -82,7 +85,7 @@ class Setup(object):
 
         # setup dialog
         self.__window = self.__builder.get_object("setup_dialog")
-        self.__window.set_title("Preferences for Cangjie")
+        self.__window.set_title("Preferences for %s" % engine.capitalize())
         self.__window.show()
 
     def run(self):
@@ -90,7 +93,7 @@ class Setup(object):
         self.__window.destroy()
 
     def on_value_changed(self, config, section, name, value, data):
-        if section == "engine/Cangjie":
+        if section == self.__config_section:
             print("[%s] Option %s was set to %s" % (section, name, value))
             print("     Data: %s" % data)
 
@@ -103,13 +106,18 @@ class Setup(object):
         self.__write(setting_name, GLib.Variant('i', model[tree_iter][0]))
 
     def __read(self, name):
-        return self.__config.get_value("engine/Cangjie", name)
+        return self.__config.get_value(self.__config_section, name)
 
     def __write(self, name, v):
-        return self.__config.set_value("engine/Cangjie", name, v)
+        return self.__config.set_value(self.__config_section, name, v)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Cangjie input method setup")
+    parser.add_argument("engine", help="Input method engine to set up",
+                        choices=("cangjie", "quick"))
+    args = parser.parse_args()
+
     locale.bindtextdomain(config.gettext_package, config.localedir)
     locale.bind_textdomain_codeset(config.gettext_package, "UTF-8")
 
@@ -125,4 +133,4 @@ if __name__ == "__main__":
         dialog.run()
         sys.exit(1)
 
-    Setup(bus).run()
+    Setup(bus, args.engine).run()
