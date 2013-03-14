Cangjians/ibus-cangjie

commit 07c6055dc341b6ea005fd44dd90a8b47b581dd4d

cangjie: clear input box on new keys after an error
fixes #22

src/engine.py
@@ -51,6 +51,7 @@ class Engine(IBus.Engine):
 
         self.current_input = ""
         self.current_radicals = ""
+        self.clear_on_next_input = False
 
         self.lookuptable = IBus.LookupTable()
         self.lookuptable.set_page_size(9)
@@ -280,16 +281,23 @@ class Engine(IBus.Engine):
         self.current_input = ""
         self.current_radicals = ""
 
+        self.clear_on_next_input = False
+
         self.update_lookup_table()
         self.update_auxiliary_text()
 
     def update_current_input(self, append=None, drop=None):
         """Update the current input."""
         if append is not None:
+            if self.clear_on_next_input:
+                self.clear_current_input()
+
             self.current_input += append
             self.current_radicals += self.cangjie.translateInputKeyToCangJie(append)
 
         elif drop is not None:
+            self.clear_on_next_input = False
+
             self.current_input = self.current_input[:-drop]
             self.current_radicals = self.current_radicals[:-drop]
 
@@ -318,6 +326,7 @@ class Engine(IBus.Engine):
 
         if num_candidates == 0:
             self.play_error_bell()
+            self.clear_on_next_input = True
 
         elif num_candidates == 1:
             self.do_select_candidate(1)

commit 8a6dca08f2bf444fb8d85c49fcafe2152d6dacf3

Really automatically clear on errors
Commit 07c6055 introduced a fix for issue #22, so that we now
automatically clear the current input on a user mistake.

This commit just makes sure we actually always do that.

Specifically, we were missing the case where we had already reached the
maximum input length.

This commit also adds a unit test, to make sure we do not regress in the
future.

src/engine.py
@@ -313,8 +313,11 @@ class Engine(IBus.Engine):
             if self.clear_on_next_input:
                 self.clear_current_input()
 
-            self.current_input += append
-            self.current_radicals += self.cangjie.translateInputKeyToCangJie(append)
+            if len(self.current_input) < self.input_max_len:
+                self.current_input += append
+                self.current_radicals += self.cangjie.translateInputKeyToCangJie(append)
+            else:
+                self.play_error_bell()
 
         elif drop is not None:
             self.clear_on_next_input = False
@@ -406,11 +409,7 @@ class EngineCangjie(Engine):
         if self.lookuptable.get_number_of_candidates():
             self.do_select_candidate(1)
 
-        if len(self.current_input) < self.input_max_len:
-            self.update_current_input(append=inputchar)
-
-        else:
-            self.play_error_bell()
+        self.update_current_input(append=inputchar)
 
         return True
 

tests/test_cangjie.py
@@ -201,3 +201,20 @@ class CangjieTestCase(unittest.TestCase):
         self.assertEqual(len(self.engine._mock_auxiliary_text), 1)
         self.assertEqual(len(self.engine._mock_committed_text), 0)
         self.assertEqual(self.engine.lookuptable.get_number_of_candidates(), 0)
+
+    def test_autoclear_on_error_max_input(self):
+        # First make an error on purpose
+        self.engine.do_process_key_event(IBus.z, 0, 0)
+        self.engine.do_process_key_event(IBus.z, 0, 0)
+        self.engine.do_process_key_event(IBus.z, 0, 0)
+        self.engine.do_process_key_event(IBus.z, 0, 0)
+        self.engine.do_process_key_event(IBus.z, 0, 0)
+        self.engine.do_process_key_event(IBus.space, 0, 0)
+        self.assertEqual(len(self.engine._mock_auxiliary_text), 5)
+        self.assertEqual(len(self.engine.canberra._mock_played_events), 1)
+
+        # Now go on inputting
+        self.engine.do_process_key_event(IBus.z, 0, 0)
+        self.assertEqual(len(self.engine._mock_auxiliary_text), 1)
+        self.assertEqual(len(self.engine._mock_committed_text), 0)
+        self.assertEqual(self.engine.lookuptable.get_number_of_candidates(), 0)